1a5e4d1e058b4d036c30f00c319cb60cca0fd281
sereypsv <valid_@tut.by>
2024-02-16 13:37:57 +0300

    feat: подключает CSS,  шрифты, поддержку препроцессора Sass, PostCSS с Preset Env

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,13 @@
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 });
