412d14abc316223934730dae66c82bdc2e2e2cbf
sereypsv <valid_@tut.by>
2024-02-16 14:23:16 +0300

    feat: устанавливает webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import './index.scss';
 
 const root = document.querySelector('#app');
 
-function renderItem(item) {
+function renderItem(item, index) {
   const li = document.createElement('li');
   li.textContent = item.title;
-  console.log('Working');
+  if (index % 2 === 0) {
+    li.style.color = 'red';
+  }
   root.append(li);
 }
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,4 +4,9 @@ const commonConfig = require('./webpack.config.common');
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    port: 3003,
+    hot: true,
+    open: true,
+  },
 });
