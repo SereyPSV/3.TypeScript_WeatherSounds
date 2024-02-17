b7f9c1b775a921098f8f8e2b2dec259344888e08
sereypsv <valid_@tut.by>
2024-02-17 20:59:13 +0300

    feature: добавляет управление звуками, окончание задания

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ import urlSunAudio from './assets/sounds/summer.mp3';
 import urlRainAudio from './assets/sounds/rain.mp3';
 import urlSnowAudio from './assets/sounds/winter.mp3';
 
+let isPlay = false;
+let isChooseSun = false;
+let isChooseRain = false;
+let isChooseSnow = false;
 const body = document.body;
 const main = body.querySelector('main');
 const background = main.querySelector('.background');
@@ -24,31 +28,71 @@ const winterButton = buttons.querySelector('.winter');
 const snowIcon = winterButton.querySelector('.snow');
 const volumeControl = content.querySelector('.volume_control');
 
+const audio = new Audio();
+
 changeBackground([summerBg, rainyBg, winterBg][Math.round(Math.random() * 2)]);
-returnIcon();
-
-function changeBackground(image) {
-  background.style.backgroundImage = `url(${image})`;
-  returnIcon();
-}
-
-summerButton.addEventListener('click', () => {
-  changeBackground(summerBg);
-  sunIcon.style.backgroundImage = `url(${urlPauseIcon})`;
-});
-
-rainyButton.addEventListener('click', () => {
-  changeBackground(rainyBg);
-  rainIcon.style.backgroundImage = `url(${urlPauseIcon})`;
-});
-
-winterButton.addEventListener('click', () => {
-  changeBackground(winterBg);
-  snowIcon.style.backgroundImage = `url(${urlPauseIcon})`;
-});
 
 function returnIcon() {
   sunIcon.style.backgroundImage = `url(${urlSunIcon})`;
   rainIcon.style.backgroundImage = `url(${urlRainIcon})`;
   snowIcon.style.backgroundImage = `url(${urlSnowIcon})`;
 }
+function isChooseReset() {
+  isChooseSun = false;
+  isChooseRain = false;
+  isChooseSnow = false;
+}
+
+function changeBackground(image) {
+  background.style.backgroundImage = `url(${image})`;
+  returnIcon();
+}
+
+const audioPlayerControl = (urlSound, isChoose, icon, urlIcon) => {
+  audio.src = urlSound;
+  if (isChoose) {
+    if (isPlay) {
+      iconChange(icon, urlIcon);
+      audio.pause();
+      isPlay = false;
+    } else {
+      iconChange(icon, urlPauseIcon);
+      audio.play();
+      isPlay = true;
+    }
+  } else {
+    returnIcon();
+    iconChange(icon, urlPauseIcon);
+    audio.play();
+    isPlay = true;
+  }
+};
+
+function iconChange(icon, urlIcon) {
+  icon.style.backgroundImage = `url(${urlIcon})`;
+}
+
+summerButton.addEventListener('click', () => {
+  changeBackground(summerBg);
+  audioPlayerControl(urlSunAudio, isChooseSun, sunIcon, urlSunIcon);
+  isChooseReset();
+  isChooseSun = true;
+});
+
+rainyButton.addEventListener('click', () => {
+  changeBackground(rainyBg);
+  audioPlayerControl(urlRainAudio, isChooseRain, rainIcon, urlRainIcon);
+  isChooseReset();
+  isChooseRain = true;
+});
+
+winterButton.addEventListener('click', () => {
+  changeBackground(winterBg);
+  audioPlayerControl(urlSnowAudio, isChooseSnow, snowIcon, urlSnowIcon);
+  isChooseReset();
+  isChooseSnow = true;
+});
+
+volumeControl.addEventListener('input', () => {
+  audio.volume = volumeControl.value;
+});
